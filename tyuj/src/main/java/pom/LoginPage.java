5b2c41597e9eb1f3d3dcd2629be79f519e049870
Bhagyalaxmi <Bhagyalaxmi@DESKTOP-BMOMR54>
2022-04-19 17:07:33 +0530

    added loginpage class

---
+++ tyuj/src/main/java/pom/LoginPage.java
@@ -0,0 +1,18 @@
+package pom;
+
+import org.testng.annotations.Test;
+
+public class LoginPage {
+	
+	@Test
+	
+	public void verifyTiltle() {
+		System.out.println("title must capital first");
+		
+		
+			
+		
+	}
+	
+
+}
